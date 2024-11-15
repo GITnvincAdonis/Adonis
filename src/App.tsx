cf6187ccd30237710d55ab16ce9bea2a8572a253
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-15 01:08:29 -0400

    mismatch in route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import Homepage from "./Routes/Homepage";
 import { Route, Routes } from "react-router-dom";
 import WebsitesPage from "./Routes/WebsitesPage";
 
-
 function App() {
   return (
     <>
       {" "}
       <Routes>
         <Route path="/">
-          <Route path={"home"} element={<Homepage></Homepage>}></Route>
+          <Route path={"/"} element={<Homepage></Homepage>}></Route>
           <Route
             path="Websites"
             element={<WebsitesPage></WebsitesPage>}
